f8c57a212f38af41744ceb8b66621a5e19eb28b3
Arley Augusto Aguiar <arley.it@icloud.com>
2024-08-26 21:09:23 -0300

    att.

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,33 @@
  var monstros = [
     {
         id: 1,
-        Name: 'Greenzord',
+        Name: 'Greenzord ⚙️',
         Height: 2.65,
-        Skills: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt cupiditate repellendus veritatis modi, laborum maxime velit ut ad voluptate maiores.',
+        Skills: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, amet quis commodi cupiditate architecto eligendi culpa harum! Suscipit, aspernatur alias qui porro dolore nam ipsum quas minima, laudantium sed eos?',
         Picture: 'https://robohash.org/189.37.78.71.png/roberto',
     },
 
     {
         id: 2,
-        Name: 'Maracujino',
+        Name: 'Maracujino ⚙️',
         Height: 3.10,
-        Skills: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt cupiditate repellendus veritatis modi, laborum maxime velit ut ad voluptate maiores.',
+        Skills: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, amet quis commodi cupiditate architecto eligendi culpa harum! Suscipit, aspernatur alias qui porro dolore nam ipsum quas minima, laudantium sed eos?',
         Picture: 'https://robohash.org/189.37.78.71.png/marciano',
     },
 
     {
         id: 3,
-        Name: 'Tomatz',
+        Name: 'Tomatz ⚙️',
         Height: 2.88,
-        Skills: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt cupiditate repellendus veritatis modi, laborum maxime velit ut ad voluptate maiores. ',
+        Skills: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, amet quis commodi cupiditate architecto eligendi culpa harum! Suscipit, aspernatur alias qui porro dolore nam ipsum quas minima, laudantium sed eos?',
         Picture: 'https://robohash.org/189.37.78.71.png/kaleb',
     },
 
     {
         id: 4,
-        Name: 'Silvestro',
+        Name: 'Silvestro ⚙️',
         Height: 2.42,
-        Skills: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt cupiditate repellendus veritatis modi, laborum maxime velit ut ad voluptate maiores. ',
+        Skills: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, amet quis commodi cupiditate architecto eligendi culpa harum! Suscipit, aspernatur alias qui porro dolore nam ipsum quas minima, laudantium sed eos?',
         Picture: 'https://robohash.org/189.37.78.71.png/james',
     }
 ]
